475049dd23ba6fd0eea5c13fa5e37014d35b40fa
ehthesam-sahi <158064387+ehthesam-sahi@users.noreply.github.com>
2026-08-27 20:38:24 +0600

    github setup

---
+++ random.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s;
+
+    s.name = "Sahi";
+    s.age = 22;
+
+    s.display();
+
+    return 0;
+}
