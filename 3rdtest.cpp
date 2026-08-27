72031cc43b0458a8cb9bd9c243f905bfa4936e75
Yashaa Islam Oishee <oishee.bd.02@gmail.com>
2026-08-27 23:04:06 +0600

    commited by oishee

--- 3rdtest.cpp
+++ 3rdtest.cpp
@@ -1,5 +1,13 @@
-print("MY name is hafsa");
-print("MY  id is 416")
-print("MY name is  SAHI");
-print("MY  id is 413")   
-adddda
+#include <iostream>
+
+int main() {
+	std::cout << "MY name is hafsa\n";
+	std::cout << "MY id is 416\n";
+	std::cout << "MY name is SAHI\n";
+	std::cout << "MY id is 413\n";
+	std::cout << "Oishee Islam\n";
+	std::cout << "17\n";
+	std::cout << "CSE\n";
+
+	return 0;
+}
